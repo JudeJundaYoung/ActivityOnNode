c61a397f1f09ff2de7c80508632c0e7eec21bc92
jackieHubc <jacket1288@gmail.com>
2017-09-23 14:41:01 -0700

    Simple version

--- test.cpp
+++ test.cpp
@@ -5,26 +5,22 @@
 #include "aon.cpp"
 
 int main(){
-	Node a = Node("A", 1);
-	Node b = Node("B", 2);
-	Node c = Node("C", 3);
-	Node d = Node("D", 4);
-	Node e = Node("E", 5);
-	Node f = Node("F", 6);
-    cout << "Hello World" << endl;
-    // a.printNode();
-    a.add_pre(&b);
-    a.add_pre(&c);
-    a.add_pre(&d);
-    a.add_suc(&e);
-    a.add_suc(&f);
-    printNoode(a);
-    Aon aon = Aon();
-    // cout << aon.nodes.size() <<endl;
-    // if (a.pres.size() == 0){
-    // 	cout << "A is a start point" << endl;
-    // }
-    // else printf("A has %lu pres\n", a.pres.size());
-    aon.insert(a);
-    aon.printAon();
+	// Node a = Node("A", 1);
+	// Node b = Node("B", 2);
+	// Node c = Node("C", 3);
+	// Node d = Node("D", 4);
+	// Node e = Node("E", 5);
+	// Node f = Node("F", 6);
+	
+	// a.printNode();
+	// a.add_pre(&b);
+	// a.add_pre(&c);
+	// a.add_pre(&d);
+	// a.add_suc(&e);
+	// a.add_suc(&f);
+	// printNoode(a);
+	Aon aon = Aon();
+	// aon.insert(a);
+	aon.createAon();
+	aon.printAon();
 }
